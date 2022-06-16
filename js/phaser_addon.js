7fee2f7d6c7d3eae9d889aad245539657c5f1580
andrey1303110 <andrey1303110@gmail.com>
2022-06-16 17:08:14 +0300

    delete unusable

--- js/phaser_addon.js
+++ js/phaser_addon.js
@@ -1,6 +1,3 @@
-var weapons = ['paper', 'rock', 'scissors'];
-var userWeapon = '';
-
 Phaser.Scene.prototype.addButton = function (x, y, key, callback, callbackContext, overFrame, outFrame) {
 	var btn = this.add.sprite(x, y, key, outFrame).setInteractive();
 	btn.on('pointerover', function (ptr, x, y) { this.setFrame(overFrame) });
